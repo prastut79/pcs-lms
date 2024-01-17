e6829e7cc384cd32846f57e5df9be412179384eb
Zero <netsos798@gmail.com>
2024-01-17 10:45:43 +0545

    ReadMe Updated

--- scripts/addAdmin.js
+++ scripts/addAdmin.js
@@ -1,25 +1,66 @@
+import { getApiError } from "@/app/api/utils";
 import { prisma } from "@/db/prisma";
 import { hash } from "bcryptjs";
 
-async function addAdmin() {
-	console.log("\n\nCreating Admin User...\n\n");
+const readline = require("readline");
 
-	const data = {
-		email: "admin@admin.com",
-		name: "Admin User",
-		phone: "9841525152",
-		role: "admin",
-		address: "Kathmandu",
-		password: await hash("admin", 8),
-	};
-	const req = await prisma.user.create({
-		data,
-	});
-	console.log("Admin created");
-	console.log("-" * 30);
-	console.log("Email: ", req.email);
-	console.log("Password: admin\n\n");
-	console.log("-" * 30);
+/**Script to add an admin user */
+async function addAdmin() {
+	try {
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
+		const prompt = (query) =>
+			new Promise((resolve) => rl.question(query, resolve));
+
+		console.log("\n\nCreating Admin User...\n\n");
+
+		const data = {
+			email: "",
+			name: "",
+			phone: "",
+			role: "admin",
+			address: "",
+			password: "",
+		};
+
+		data.email = String(await prompt("Enter Email: "))
+			.trim()
+			.toLowerCase();
+		data.name = String(await prompt("Enter Name: ")).trim();
+		data.phone = String(await prompt("Enter Phone Number: ")).trim();
+		data.address = String(await prompt("Enter Address: ")).trim();
+
+		while (true) {
+			const password = String(
+				await prompt("Enter your password: ")
+			).trim();
+			const rePassword = String(
+				await prompt("Re-enter the password: ")
+			).trim();
+
+			if (password !== rePassword) {
+				console.log("\nPasswords must match. Try Again\n");
+			} else {
+				data.password = await hash(password, 8);
+				break;
+			}
+		}
+
+		const req = await prisma.user.create({
+			data,
+		});
+		console.log();
+		console.log("-".repeat(40));
+		console.log("Admin created. You can now login");
+		console.log("-".repeat(40));
+		console.log();
+	} catch (e) {
+		console.log("\nAn Error Occured: ");
+		console.log(getApiError(e));
+	}
+	process.exit();
 }
 
 addAdmin();
